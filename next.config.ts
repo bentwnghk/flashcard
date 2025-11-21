4bcb0ac1c56f920ab25393b7c35f8ef50e600c4b
Ben <mr.ng@mystudies.net>
2025-11-21 21:21:42 +0800

    fix error

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js'],
-  },
+  serverExternalPackages: ['@supabase/supabase-js'],
   images: {
-    domains: ['images.unsplash.com', 'images.unsplash.com'],
     remotePatterns: [
       {
         protocol: 'https',
